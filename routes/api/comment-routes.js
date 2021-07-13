ad4b9963fb203621fc3fd62ccd6975756cc0fa25
Ronnie Rios <ronniesmail14@gmail.com>
2021-07-13 15:40:51 -0500

    added indexeddb for offline saving

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,13 +1,21 @@
 const router = require('express').Router();
-const { addComment, removeComment, addReply, removeReply } = require('../../controllers/comment-controllers');
+const {
+  addComment,
+  removeComment,
+  addReply,
+  removeReply
+} = require('../../controllers/comment-controllers');
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
+
 // /api/comments/<pizzaId>/<commentId>
 router
-    .router('/:pizzaId/:commentId')
-    .put(addReply)
-    .delete(removeComment)
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment);
 
+// /api/comments/<pizzaId>/<commentId>/<replyId>
 router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
+
 module.exports = router;
